27873b8be61f6f6ab87f5e86195f593fe6f92a16
manideep <manideepanasuri@gmail.com>
2025-06-12 11:52:22 +0530

    updated for s3 storage

--- makevideo/helpers.py
+++ makevideo/helpers.py
@@ -11,8 +11,8 @@ def upload_file_to_minio(file_bytes, filename):
         aws_access_key_id=settings.MINIO_STORAGE['ACCESS_KEY'],
         aws_secret_access_key=settings.MINIO_STORAGE['SECRET_KEY'],
         config=Config(signature_version='s3v4'),
-        region_name='us-east-1',  # MinIO uses 'us-east-1' by default, can be anything
-        verify=False,  # For self-signed certs, set True if using real SSL
+        region_name='ap-south-1',  # MinIO uses 'us-east-1' by default, can be anything
+        verify=True,  # For self-signed certs, set True if using real SSL
     )
     if isinstance(file_bytes, BytesIO):
         file_obj = file_bytes
@@ -22,7 +22,7 @@ def upload_file_to_minio(file_bytes, filename):
     s3_client.upload_fileobj(file_obj, settings.MINIO_STORAGE['BUCKET_NAME'], filename)
 
     # Return the public URL of the uploaded file if bucket is public
-    url = f"{settings.MINIO_STORAGE['ENDPOINT']}/{settings.MINIO_STORAGE['BUCKET_NAME']}/{filename}"
+    url = f"{settings.MINIO_STORAGE['ENDPOINT']}{settings.MINIO_STORAGE['BUCKET_NAME']}/{filename}"
     return url
 
 def delete_file_from_minio(filename:str)->bool:
@@ -33,8 +33,8 @@ def delete_file_from_minio(filename:str)->bool:
             aws_access_key_id=settings.MINIO_STORAGE['ACCESS_KEY'],
             aws_secret_access_key=settings.MINIO_STORAGE['SECRET_KEY'],
             config=Config(signature_version='s3v4'),
-            region_name='us-east-1',  # MinIO uses 'us-east-1' by default, can be anything
-            verify=False,  # For self-signed certs, set True if using real SSL
+            region_name='ap-south-1',  # MinIO uses 'us-east-1' by default, can be anything
+            verify=True,  # For self-signed certs, set True if using real SSL
         )
         s3_client.delete_object(Bucket=settings.MINIO_STORAGE['BUCKET_NAME'], Key=filename)
         return True
